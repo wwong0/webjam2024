888fb38ebeb5256176981d8e307c1294507d2eb2
wwong0 <120128836+wwong0@users.noreply.github.com>
2024-11-19 18:05:10 -0800

    a

--- test.py
+++ test.py
@@ -3,5 +3,5 @@ import pathlib
 from datetime import datetime
 import json
 
-with open(pathlib.Path('sample_weekly_output.txt'), 'w', encoding= 'utf-8') as f:
-    json.dump(weekMenu.week_menu_dict(datetime.today()), f)
+with open(pathlib.Path('sample_weekly_output.json'), 'w', encoding= 'utf-8') as f:
+    json.dump(weekMenu.week_menu_dict(datetime.today()), f, indent = 4)
