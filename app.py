e164207b0b8ef7a2248c2d0b810e0713d622075a
BigBrainBoiss <what02.rbxl@gmail.com>
2024-11-22 07:49:13 -0800

    Branched app.py and data.json

--- app.py
+++ app.py
@@ -2,15 +2,18 @@ from flask import Flask, render_template, request, redirect, url_for, jsonify
 import fuzzywuzzy.process
 app = Flask(__name__)
 print(app.template_folder)
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/search', methods = ['POST'])
+
+@app.route('/search', methods=['POST'])
 def fuzzy_search():
     query = request.json.get('query', '')
     if query != '':
-        results = fuzzywuzzy.process.extract(query, data, limit = 10)
+        results = fuzzywuzzy.process.extract(query, data, limit=10)
         matches = [result[0] for result in results]
     else:
         matches = []
