e8d87f8b87f612d1e63711c7d167ffeda1cbf366
William Wong <120128836+wwong0@users.noreply.github.com>
2024-11-22 01:38:19 -0800

    html

--- app.py
+++ app.py
@@ -1,18 +1,10 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify
 
 app = Flask(__name__)
-
-header_text = "MONDAY"
-
+print(app.template_folder)
 @app.route('/')
 def index():
-    return render_template('index.html', header_text=header_text)
-
-@app.route('/change-header', methods=['GET'])
-def change_header():
-    global header_text
-    header_text = "TUESDAY"
-    return redirect(url_for('index'))
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
