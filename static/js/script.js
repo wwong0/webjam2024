83491e1086d33ee928e2baed72e52bb6dd045329
William Wong <120128836+wwong0@users.noreply.github.com>
2024-11-22 07:47:58 -0800

    i love javascript

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
+    /*
+    //search bar
     const searchInput = document.getElementById('searchInput');
     const resultsDiv = document.getElementById('results');
 
@@ -18,15 +20,49 @@ document.addEventListener('DOMContentLoaded', function () {
             .then(function (data) {
                 resultsDiv.innerHTML = '';
 
-
                 data.matches.forEach(function (match) {
-                    var p = document.createElement('p');
+                    /*let p = document.createElement('p');
                     p.textContent = match;
-                    resultsDiv.appendChild(p);
+                    resultsDiv.appendChild(p);*/
                 });
             })
-            .catch(function (error) {
-                console.error('Error:', error);
-            });
     });
+
+
+    const dayCols = document.querySelectorAll('.day-col');
+
+    dayCols.forEach(function (dayCol) {
+        dayCol.addEventListener('click', function () {
+            clearColSelection(dayCols);
+            selectCol(dayCol);
+        })
+    })
 });
+
+let daySelectedList = {
+    'monday': false,
+    'tuesday': false,
+    'wednesday': false,
+    'thursday': false,
+    'friday': false,
+    'saturday': false,
+    'sunday': false,
+}
+
+function selectCol(col) {
+    if (col.getAttribute('day') === 'monday') {
+        daySelectedList[col.getAttribute('day')] = true;
+    }
+    col.classList.add('selected')
+}
+
+function clearColSelection(dayCols) {
+    for (const key in daySelectedList) {
+        daySelectedList[key] = false;
+    }
+
+    dayCols.forEach(function (col) {
+        col.classList.remove('selected')
+    })
+
+}
